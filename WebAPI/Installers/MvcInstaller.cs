0787f80c8075ba378b281c575fe39e64ebf23ece
tgrzywacz <tomasz.grzywacz@altkomsoftware.pl>
2021-07-03 09:43:08 +0200

    part 1 Wstrzykiwanie zależności i rejestracja serwisów

---
+++ WebAPI/Installers/MvcInstaller.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces;
+using Application.Mappings;
+using Application.Services;
+
+using Domain.Interfaces;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Installers
+{
+    public class MvcInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddScoped<IPostRepository, PostRepository>();
+            services.AddScoped<IPostService, PostService>();
+
+            services.AddSingleton(AutoMapperConfig.Initialize());
+
+            services.AddControllers();
+        }
+
+    }
+}

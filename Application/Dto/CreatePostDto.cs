a4fdee325abcd3d6ebc6f94dbb878fc26a099755
tgrzywacz <tomasz.grzywacz@altkomsoftware.pl>
2021-07-03 08:41:37 +0200

    Tworzenie  zasobu (metoda POST)

---
+++ Application/Dto/CreatePostDto.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dto
+{
+    public class CreatePostDto
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CreatePostDto, Post>();
+        }
+    }
+}
--- Application/Services/PostService.cs
+++ Application/Services/PostService.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Dto;
 using Application.Interfaces;
 using AutoMapper;
+using Domain.Entities;
 using Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,7 @@ namespace Application.Services
             _postRepository = postRepository;
             _mapper = mapper;
         }
+
 
         public IEnumerable<PostDto> GetAllPosts()
         {
@@ -44,6 +46,18 @@ namespace Application.Services
             //    Content = post.Content
             //};
 
+            return _mapper.Map<PostDto>(post);
+        }
+
+        public PostDto AddNewPost(CreatePostDto newPost)
+        {
+            if (string.IsNullOrEmpty(newPost.Title))
+            {
+                throw new Exception("Post can not have an empty title.");
+            }
+
+            var post = _mapper.Map<Post>(newPost);
+            _postRepository.Add(post);
             return _mapper.Map<PostDto>(post);
         }
     }

be3ed4ced8db55897bd959dcfbe9c755aea19ece
tgrzywacz <tomasz.grzywacz@altkomsoftware.pl>
2021-07-03 11:04:15 +0200

    part 1 Manipulowanie danymi (EF Core)

---
+++ Infrastructure/Data/BloggerContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class BloggerContext : DbContext
+    {
+        public BloggerContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
